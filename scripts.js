05ff285fca1e1e7897024bb966a9ee640f100cea
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-07-02 17:32:23 -0400

    refactor: Modify revenue line graph"

--- scripts.js
+++ scripts.js
@@ -1,39 +1,46 @@
 function createRevenueChart() {
+    const revenueChartNode = document.getElementById('revenue-chart');
+
+    // Axes values
     const xValues = ['1st', '2nd', '3rd', '4th'];
     const yValues = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-    const revenueData = {
-        labels: xValues,
-        datasets: [{
-            label: 'Quarterly Revenue',
-            data: [20, 30, 75, 60],
-            fill: false,
-            borderColor: 'rgb(20, 208, 42)',
-            backgroundColor: 'white',
-            tension: 0.1
-        }]
-    };
-
-    return new Chart('revenue-chart', {
+    new Chart(revenueChartNode, {
         type: 'line',
-        data: revenueData,
+        data: {
+            labels: xValues,
+            datasets: [{
+                label: 'Quarterly Revenue',
+                data: [20, 30, 75, 60],
+            }],
+        },
         options: {
             scales: {
-                xAxes: [{
-                    scaleLabel: {
+                y: {
+                    min: 10,
+                    max: 100,
+                    tick: {
+                        stepSize: 10,
+                    },
+
+                    title: {
                         display: true,
-                        labelString: 'Quarter'
+                        text: 'Revenue ($ mil)'
                     }
-                }],
-                yAxes: [{
-                    scaleLabel: {
+                },
+
+                x : {
+                    title: {
                         display: true,
-                        labelString: 'Revenue ($ / mil)'
-                    }
-                }],
+                        text: 'Yearly Quarter',
+                    },
+                },
             },
-        },
-    });
+
+            backgroundColor: 'rgb(17, 4, 255)',
+            borderColor: 'rgb(98, 10, 221)',
+        }
+    })
 }
 
-const revenueChart = createRevenueChart();
+createRevenueChart();
